4a8d8f40083208fb2d5949c97d02a81b09fde5eb
GeovanGoes <geovansilvagoes@gmail.com>
2019-09-30 21:06:15 -0300

    starting second part

---
+++ angular-seis/alurapic/src/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+
+
+
+
+@NgModule({
+    exports: [SignInComponent]
+})
+export class HomeModule {
+
+}
